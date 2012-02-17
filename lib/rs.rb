35fb696c398a26305efc9a2fccabe32815a065b7
Dimitri Kurashvili <dimitri@c12.ge>
2012-02-18 01:12:32 +0400

    UTF-8 prepends using magic_encoding

--- lib/rs.rb
+++ lib/rs.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 require 'savon'
 require 'rs/version'
 require 'rs/sys'
--- spec/rs/sys_spec.rb
+++ spec/rs/sys_spec.rb
@@ -1,4 +1,4 @@
-# encoding: utf-8
+# -*- encoding : utf-8 -*-
 require 'spec_helper'
 
 IP_PATTERN = /^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$/
