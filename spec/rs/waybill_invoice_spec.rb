5413362e4d8092c38a6f774b82f6e97f6c601213
Dimitri Kurashvili <dimitri@c12.ge>
2012-03-16 10:05:23 +0400

    open user spec and waybill_invoice manipulations

---
+++ spec/rs/waybill_invoice_spec.rb
@@ -0,0 +1,17 @@
+# -*- encoding : utf-8 -*-
+
+require 'spec_helper'
+require 'rs'
+
+describe 'save invoice (only for closed waybill)' do
+  before(:all) do
+    @waybill = waybill_skeleton
+    RS.save_waybill(@waybill, RS.su_params)
+    RS.activate_waybill(RS.su_params.merge('waybill_id' => @waybill.id))
+    RS.close_waybill(RS.su_params.merge('waybill_id' => @waybill.id))
+    RS.save_waybill_invoice(@waybill, RS.su_params)
+  end
+  subject { @waybill }
+  its(:invoice_id) { should_not be_nil }
+  its(:invoice_id) { should > 0 }
+end
