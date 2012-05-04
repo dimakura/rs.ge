2a025fc5455e89038495545c8f2ce34e081012b4
Dimitri Kurashvili <dimitri@c12.ge>
2012-05-04 12:12:25 +0400

    send_invoice works now

--- spec/rs/waybill_vat_spec.rb
+++ spec/rs/waybill_vat_spec.rb
@@ -6,17 +6,13 @@ describe 'კლიენტი დღგ-ს განაკვეთით' do
     @waybill = waybill_skeleton
     @waybill.buyer_tin = '204383176'
     @waybill.buyer_name = 'შსს საკადრო და ორგანიზაციული უზრუნველყოფის დეპარტ. საფინანსო-სამეურნეო უზრუნველყოფის მთავარი სამმ-ლო'
-    @waybill.items[0].vat_type = RS::Waybill::VAT_NONE
+    @waybill.items[0].vat_type = RS::Waybill::VAT_COMMON
     RS.save_waybill(@waybill, RS.su_params)
     RS.activate_waybill(RS.su_params.merge('waybill_id' => @waybill.id))
     RS.close_waybill(RS.su_params.merge('waybill_id' => @waybill.id))
-    #Savon.log = true
     @error = RS.save_waybill_invoice(@waybill, RS.su_params)
-    #Savon.log = false
     @waybill = RS.get_waybill(RS.su_params.merge('waybill_id' => @waybill.id))
-    #puts "#{@waybill.number} -- #{@waybill.invoice_id}"
   end
   subject { @error }
-  it { should == 0 }
+  it { should == 1 }
 end
-
