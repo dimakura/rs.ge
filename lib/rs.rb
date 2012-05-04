cd5878cd18c06274a2519eb2b01024e6de6562c8
Dimitri Kurashvili <dimitri@c12.ge>
2012-05-04 11:15:18 +0400

    add VAT constants

--- lib/rs.rb
+++ lib/rs.rb
@@ -8,3 +8,12 @@ require 'rs/models/waybill_unit'
 # requests
 require 'rs/requests/base_request'
 require 'rs/requests/waybill_unit_request'
+
+module RS
+  # This constant indicates 18% vat tariff.
+  VAT_COMMON = 0
+  # This constant indicates 0% vat tariff.
+  VAT_ZERO   = 1
+  # This constant indicates that no vat tariff applies.
+  VAT_NONE   = 2
+end
