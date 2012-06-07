ee8659778b93c1a61bb1a98960df4187ee223669
Dimitri Kurashvili <dimitri@c12.ge>
2012-06-07 16:56:59 +0400

    getting transport types

--- lib/rs/requests/dict_request.rb
+++ lib/rs/requests/dict_request.rb
@@ -21,7 +21,22 @@ module RS
     WAYBILL_TYPE_SUBWB => 'ქვე-ზედნადები'
   }
 
+  TRANS_VEHICLE = 1
+  TRANS_RAILWAY = 2
+  TRANS_AIR     = 3
+  TRANS_OTHER   = 4
+  TRANS_VEHICLE_FRGN = 6
+
+  TRANSPORT_TYPES = {
+    TRANS_VEHICLE => 'საავტომობილო',
+    TRANS_RAILWAY => 'სარკინიგზო',
+    TRANS_AIR     => 'საავიაციო',
+    TRANS_OTHER   => 'სხვა',
+    TRANS_VEHICLE_FRGN => 'საავტომობილო - უცხო ქვეყნის'
+  }
+
   class DictionaryRequest < BaseRequest
+
     include Singleton
 
     # Returns RS.GE units.
@@ -52,6 +67,20 @@ module RS
       types
     end
 
+    # Returns RS.GE transport types.
+    def transport_types(opts)
+      validate_presence_of(opts, :su, :sp)
+      response = waybill_client.request 'get_trans_types' do
+        soap.body = { 'su' => opts[:su], 'sp' => opts[:sp] }
+      end
+      resp = response.to_hash[:get_trans_types_response][:get_trans_types_result][:transport_types][:transport_type]
+      types = {}
+      resp.each do |type|
+        types[type[:id].to_i] = type[:name]
+      end
+      types
+    end
+
     # Returns name by given TIN number.
     def get_name_from_tin(params)
       validate_presence_of(params, :su, :sp, :tin)
@@ -68,4 +97,5 @@ module RS
       RS::DictionaryRequest.instance
     end
   end
+
 end
--- spec/requests/dict_spec.rb
+++ spec/requests/dict_spec.rb
@@ -28,6 +28,16 @@ describe 'getting waybill types' do
   its(:size) { should == RS::WAYBILL_TYPES.size }
 end
 
+describe 'getting transport types' do
+  before(:all) do
+    @types = RS.dict.transport_types(SU_PARAMS)
+  end
+  subject { @types }
+  it { should_not be_nil }
+  it { should_not be_empty }
+  its(:size) { should == RS::TRANSPORT_TYPES.size }
+end
+
 describe 'getting payer name from tin' do
   before(:all) do
     @name = RS.dict.get_name_from_tin(SU_PARAMS.merge(tin: '02001000490'))
