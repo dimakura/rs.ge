b68bd0a9c1cd683e090f4b83a1a0975386ba0b75
Dimitri Kurashvili <dimitri@c12.ge>
2012-06-11 10:45:28 +0400

    adding helper to specs

--- spec/requests/waybill_spec.rb
+++ spec/requests/waybill_spec.rb
@@ -1,42 +1,6 @@
 # -*- encoding : utf-8 -*-
 require 'spec_helper'
 
-def create_waybill(opts = {})
-  RS::Waybill.new(id: opts[:id] || 0,
-    type: opts[:type] || RS::WAYBILL_TYPE_TRANS,
-    status: opts[:status] || RS::Waybill::STATUS_SAVED,
-    seller_id: USER01[:payer], seller_tin: USER01[:payer_tin], seller_name: USER01[:payer_name],
-    buyer_tin: opts[:buyer_tin] || '02001000490',
-    buyer_name: opts[:buyer_name] || 'დიმიტრი ყურაშვილი',
-    check_buyer_tin: true,
-    seller_info: opts[:seller_info], buyer_info: opts[:buyer_info],
-    driver_tin: opts[:dirver_tin] || '02001000490',
-    driver_name: opts[:dirver_name] || 'დიმიტრი ყურაშვილი',
-    check_driver_tin: true,
-    start_address: opts[:start_address] || 'ქ. აბაშა, კაჭარავას 35',
-    end_address: opts[:end_address] || 'ქ. სენაკი, თავისუფლების 10',
-    transportation_cost: opts[:transportation_cost] || 0,
-    transportation_cost_payer: opts[:transportation_cost_payer] || 1,
-    transport_type_id: opts[:transport_type_id] || RS::TRANS_VEHICLE,
-    car_number: opts[:car_number] || 'wdw842',
-    comment: opts[:comment], user_id: USER01[:id],
-    items: opts[:items]
-  )
-end
-
-def create_item(opts = {})
-  RS::WaybillItem.new(id: opts[:id] || 0,
-    bar_code: opts[:bar_code] || '001',
-    prod_name: opts[:prod_name] || 'Tomato',
-    unit_id: opts[:unit_id] || RS::UNIT_OTHERS,
-    unit_name: opts[:unit_name] || 'kg',
-    quantity: opts[:quantity] || 1,
-    price: opts[:price] || 1,
-    vat_type: opts[:vat_type] || RS::VAT_COMMON,
-    delete: false
-  )
-end
-
 describe 'Save waybill' do
   before(:all) do
     items = [create_item(bar_code: '001', prod_name: 'პამიდორი', price: 2, quantity: 5), create_item(bar_code: '002', prod_name: 'კიტრი', price: 3, quantity: 10)]
@@ -182,7 +146,7 @@ describe 'Close waybill' do
     its(:activate_date) { should_not be_nil }
     its(:delivery_date) { should_not be_nil }
   end
-  context 'with delivery_date' do
+  context 'without delivery_date' do
     before(:all) do
       items = [
         create_item(bar_code: '001', prod_name: 'iPhone 4S 3G/32GB', price: 1800, quantity: 2),
