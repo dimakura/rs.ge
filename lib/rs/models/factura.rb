98bc447cb622c8c0e127d53f59ea79265f7128d0
Dimitri Kurashvili <dimitri@c12.ge>
2012-09-21 15:52:29 +0400

    adding invoice statuses

--- lib/rs/models/factura.rb
+++ lib/rs/models/factura.rb
@@ -1,9 +1,18 @@
 # -*- encoding : utf-8 -*-
 
 class RS::Factura
+  # Source invoice -- პირველადი ა/ფ
   STATUS_START = 0
   STATUS_SENT  = 1
   STATUS_CONFIRMED = 2
+  STATUS_CORRECTED = 3
+  # Correction invoice -- კორექტირებული ა/ფ
+  STATUS_CORR_START = 4
+  STATUS_CORR_SENT = 5
+  STATUS_CORR_CONFIRMED = 8
+  # Canceled -- გაუქმებულია
+  STATUS_CANCELED = 6
+  STATUS_CONFIRMED_CANCELED = 7
 
   attr_accessor :id
   attr_accessor :index
