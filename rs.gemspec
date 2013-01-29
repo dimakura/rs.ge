9fe46f3be069ebfa3b8231a2aa7d4f238536e880
Dimitri Kurashvili <dimitri@c12.ge>
2013-01-29 12:06:07 +0400

    fixing library dependencies

--- rs.gemspec
+++ rs.gemspec
@@ -21,5 +21,6 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'rspec', '~> 2'
   s.add_runtime_dependency 'savon', '~> 1.0'
   s.add_runtime_dependency 'prawn', '~>0.12'
-  s.add_runtime_dependency 'c12-commons', '~> 0.0.5'
+  s.add_runtime_dependency 'c12-commons', '~> 0.1.0'
+  s.add_runtime_dependency 'activesupport', '~> 3.2.0'
 end
--- spec/requests/factura_spec.rb
+++ spec/requests/factura_spec.rb
@@ -112,18 +112,19 @@ describe 'get factura' do
   end
   context do
     before(:all) do
-      @id = 16953719
+      @id = 22813602
       @factura = RS.fact.get_factura(id: @id)
     end
     subject { @factura }
     it { should_not be_nil }
     its(:id) { should == @id }
     its(:index) { should == 'ეა-36'}
-    its(:number) { should == 269554 }
+    #its(:number) { should == 269554 }
+    its(:number) { should be_nil }
     its(:operation_date) { should_not be_nil }
     its(:registration_date) { should_not be_nil }
     its(:seller_id) { should == 731937 }
-    its(:status) { should == RS::Factura::STATUS_SENT }
+    its(:status) { should == RS::Factura::STATUS_START }
     its(:correction_of) { should be_nil }
   end
   context do
