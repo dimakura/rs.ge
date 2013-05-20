3b742c6cbdf440511cb6d9dc5d326e08c213989c
Dimitri Kurashvili <dimakura@gmail.com>
2013-05-20 11:43:16 +0400

    is_vat_payer method

--- lib/rs/dict.rb
+++ lib/rs/dict.rb
@@ -2,16 +2,19 @@
 module RS
   def get_oranizaton_info_from_tin(opts)
     validate_presence_of(opts, :su, :sp, :tin, :user_id)
-
-puts opts
-
-    response = invoice_client.call(:get_un_id_from_tin, message: { user_id: opts[:user_id], tin: opts[:tin], su: opts[:su], sp: opts[:sp] }).to_hash
-
-puts response
-
+    message = { 'user_id' => opts[:user_id], 'tin' => opts[:tin], 'su' => opts[:su], 'sp' => opts[:sp] }
+    # puts message
+    response = invoice_client.call(:get_un_id_from_tin, message: message).to_hash
     id = response[:get_un_id_from_tin_response][:get_un_id_from_tin_result]
     name = response[:get_un_id_from_tin_response][:name]
     { id: id, name: name }
+  end
+
+  def is_vat_payer(opts)
+    validate_presence_of(opts, :su, :sp, :payer_id)
+    message = { 'su' => opts[:su], 'sp' => opts[:sp], 'un_id' => opts[:payer_id] }
+    response = waybill_client.call(:is_vat_payer, message: message).to_hash
+    response[:is_vat_payer_response][:is_vat_payer_result]
   end
 
   def get_name_from_tin(opts)
@@ -21,5 +24,6 @@ puts response
   end
 
   module_function :get_oranizaton_info_from_tin
+  module_function :is_vat_payer
   module_function :get_name_from_tin
 end
