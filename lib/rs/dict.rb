a929a154dccfe615e5ca9040e2d5a72a69dab174
Dimitri Kurashvili <dimakura@gmail.com>
2013-05-23 15:55:23 +0400

    getting units

--- lib/rs/dict.rb
+++ lib/rs/dict.rb
@@ -22,6 +22,13 @@ module RS
     response.to_hash[:get_name_from_tin_response][:get_name_from_tin_result]
   end
 
+  def get_units(opts)
+    validate_presence_of(opts, :su, :sp)
+    response = waybill_client.call(:get_waybill_units, message: { 'su' => opts[:su], 'sp' => opts[:sp] })
+    resp = response.to_hash[:get_waybill_units_response][:get_waybill_units_result][:waybill_units][:waybill_unit]
+    Hash[resp.map { |x| [ x[:id].to_i, x[:name] ] }]
+  end
+
   module_function :get_oranizaton_info_from_tin
   module_function :is_vat_payer
   module_function :get_name_from_tin
