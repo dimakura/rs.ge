5b249ec1fbf45cbf4aae961f4132f8c3c4d15463
Dimitri Kurashvili <dimakura@gmail.com>
2013-09-09 14:48:05 +0400

    testing "check user" method

--- lib/rs/sys.rb
+++ lib/rs/sys.rb
@@ -4,13 +4,6 @@ module RS
     waybill_client.call(:what_is_my_ip).body.to_hash[:what_is_my_ip_response][:what_is_my_ip_result]
   end
 
-  def create_user(opts)
-    validate_presence_of(opts, :username, :password, :ip, :name, :su, :sp)
-    message = {'user_name' => opts[:username], 'user_password' => opts[:password], 'ip' => opts[:ip], 'name' => opts[:name], 'su' => opts[:su], 'sp' => opts[:sp]}
-    response = waybill_client.call :create_service_user, message: message
-    response.to_hash[:create_service_user_response][:create_service_user_result]
-  end
-
   def update_user(opts)
     validate_presence_of(opts, :username, :password, :ip, :name, :su, :sp)
     response = waybill_client.call :update_service_user, message: {'user_name' => opts[:username], 'user_password' => opts[:password], 'ip' => opts[:ip], 'name' => opts[:name], 'su' => opts[:su], 'sp' => opts[:sp]}
@@ -28,7 +21,6 @@ module RS
   end
 
   module_function :what_is_my_ip
-  module_function :create_user
   module_function :update_user
   module_function :check_user
 end
--- test/sys_test.rb
+++ test/sys_test.rb
@@ -9,16 +9,22 @@ class SystemTest < Test::Unit::TestCase
     assert ip =~ /^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$/
   end
 
-  def test_create_user
-    su = "myuser_#{rand(1000)}"
-    sp = '123456'
-    assert RS.create_user(TEST_USER.merge(ip: RS.what_is_my_ip, name: 'test user', su: su, sp: sp)) == true
-    # check_result = RS.check_user(su: su, sp: sp)
-    # assert_equal TEST_PAYER_ID, check_result[:payer]
-    # user_id = check_result[:user]
-    # assert user_id > 0
-    # assert_nil RS.check_user(su: su, sp: 'wrongpassword')
-    # assert RS.update_user(TEST_USER.merge(ip: RS.what_is_my_ip, name: 'test user', su: su, sp: 'newpassword')) == true
-    # assert_equal user_id, RS.check_user(su: su, sp: 'newpassword')[:user]
+  # def test_create_user
+  #   su = "myuser_#{rand(1000)}"
+  #   sp = '123456'
+  #   assert RS.create_user(TEST_USER.merge(ip: RS.what_is_my_ip, name: 'test user', su: su, sp: sp)) == true
+  #   # check_result = RS.check_user(su: su, sp: sp)
+  #   # assert_equal TEST_PAYER_ID, check_result[:payer]
+  #   # user_id = check_result[:user]
+  #   # assert user_id > 0
+  #   # assert_nil RS.check_user(su: su, sp: 'wrongpassword')
+  #   # assert RS.update_user(TEST_USER.merge(ip: RS.what_is_my_ip, name: 'test user', su: su, sp: 'newpassword')) == true
+  #   # assert_equal user_id, RS.check_user(su: su, sp: 'newpassword')[:user]
+  # end
+
+  def test_check_user
+    data = RS.check_user(TEST_SU)
+    assert_equal 731937, data[:payer]
+    assert_equal 20155, data[:user]
   end
 end
