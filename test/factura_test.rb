fb263c47f63c1a7a49f613402fd1f227ea05112b
Dimitri Kurashvili <dimitri@c12.ge>
2013-09-10 23:33:40 +0400

    item operations test separated from factura cycle

--- test/factura_test.rb
+++ test/factura_test.rb
@@ -2,7 +2,7 @@
 require 'test_helper'
 
 class FacturaTest < Test::Unit::TestCase
-  def test_create_factura
+  def test_factura_items
     factura = RS::Factura.new(date: Time.now, seller_id: TEST_PAYER_ID)
     factura_item = RS::FacturaItem.new(factura: factura, good: 'potato', unit: 'kg', quantity: 10, amount: 100, vat: 18)
     assert_nil factura.id
@@ -15,6 +15,32 @@ class FacturaTest < Test::Unit::TestCase
     assert RS.save_factura_item(factura_item, TEST_SU.merge(user_id: TEST_USER_ID))
     refute_nil factura_item.id
     assert factura_item.id > 0
+    # get factura items
+    items = RS.get_factura_items(TEST_SU.merge(user_id: TEST_USER_ID, id: factura.id))
+    assert_equal 1, items.size
+    # delete factura item
+    assert RS.delete_factura_item(TEST_SU.merge(user_id: TEST_USER_ID, id: items.first.id, factura_id: factura.id))
+    items2 = RS.get_factura_items(TEST_SU.merge(user_id: TEST_USER_ID, id: factura.id))
+    assert items2.empty?
+  end
+
+  def test_factura_cycle
+    factura = RS::Factura.new(date: Time.now, seller_id: TEST_PAYER_ID)
+    factura_item = RS::FacturaItem.new(factura: factura, good: 'potato', unit: 'kg', quantity: 10, amount: 100, vat: 18)
+    assert_nil factura.id
+    assert_nil factura_item.id
+    # save factura
+    assert RS.save_factura(factura, TEST_SU.merge(buyer_tin: '12345678910', user_id: TEST_USER_ID))
+    refute_nil factura.id
+    assert factura.id > 0
+    # save factura item
+    assert RS.save_factura_item(factura_item, TEST_SU.merge(user_id: TEST_USER_ID))
+    refute_nil factura_item.id
+    assert factura_item.id > 0
+    # get factura items
+    items = RS.get_factura_items(TEST_SU.merge(user_id: TEST_USER_ID, id: factura.id))
+    assert_equal 1, items.size
+    # TODO: confirm factura
   end
 
   def test_get_factura_and_items
