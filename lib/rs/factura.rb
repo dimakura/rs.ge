95740d01288238f742f827338ce97a94df28b4c2
Dimitri Kurashvili <dimakura@gmail.com>
2013-09-10 12:12:20 +0400

    save_factura_item method

--- lib/rs/factura.rb
+++ lib/rs/factura.rb
@@ -5,6 +5,11 @@ module RS
     attr_accessor :seller_id, :buyer_id
   end
 
+  class FacturaItem < RS::Initializable
+    attr_accessor :id, :factura, :good, :unit
+    attr_accessor :amount, :quantity, :vat, :excise_amount, :excise_code
+  end
+
   def save_factura(factura, opts = {})
     validate_presence_of(opts, :user_id, :su, :sp)
     if opts[:buyer_tin].present?
@@ -17,7 +22,6 @@ module RS
       'user_id' => opts[:user_id], 'invois_id' => (factura.id || 0), 'operation_date' => (factura.date || Time.now),
       'seller_un_id' => factura.seller_id, 'buyer_un_id' => factura.buyer_id,
       'overhead_no' => '', 'overhead_dt' => format_date(Time.now), # those two parameters are not used actually!
-      # , 'b_s_user_id' => nil,
       'su' => opts[:su], 'sp' => opts[:sp],
     }).to_hash
     if response[:save_invoice_response][:save_invoice_result]
@@ -28,5 +32,24 @@ module RS
     end
   end
 
+  def save_factura_item(factura_item, opts = {})
+    factura = factura_item.factura
+    validate_presence_of(opts, :user_id, :su, :sp)
+    raise 'factura not saved' if (factura.id.blank? or factura.id == 0)
+    response = invoice_client.call(:save_invoice_desc, message: {
+      'user_id' => opts[:user_id], 'id' => (factura_item.id || 0), 'su' => opts[:su], 'sp' => opts[:sp],
+      'invois_id' => factura.id, 'goods' => factura_item.good, 'g_unit' => factura_item.unit, 'g_number' => factura_item.quantity,
+      'full_amount' => factura_item.amount, 'drg_amount' => factura_item.vat,
+      'aqcizi_amount' => (factura_item.excise_amount || 0), 'akciz_id' => (factura_item.excise_code || 0)
+    }).to_hash
+    if response[:save_invoice_desc_response][:save_invoice_desc_result]
+      factura_item.id = response[:save_invoice_desc_response][:id].to_i
+      true
+    else
+      false
+    end
+  end
+
   module_function :save_factura
+  module_function :save_factura_item
 end
--- test/factura_test.rb
+++ test/factura_test.rb
@@ -4,9 +4,16 @@ require 'test_helper'
 class FacturaTest < Test::Unit::TestCase
   def test_create_factura
     factura = RS::Factura.new(date: Time.now, seller_id: TEST_PAYER_ID)
+    factura_item = RS::FacturaItem.new(factura: factura, good: 'potato', unit: 'kg', quantity: 10, amount: 100, vat: 18)
     assert_nil factura.id
-    RS.save_factura(factura, TEST_SU.merge(buyer_tin: '12345678910', user_id: TEST_USER_ID))
+    assert_nil factura_item.id
+    # save factura
+    assert RS.save_factura(factura, TEST_SU.merge(buyer_tin: '12345678910', user_id: TEST_USER_ID))
     refute_nil factura.id
     assert factura.id > 0
+    # save factura item
+    assert RS.save_factura_item(factura_item, TEST_SU.merge(user_id: TEST_USER_ID))
+    refute_nil factura_item.id
+    assert factura_item.id > 0
   end
 end
