fcbee92de3da85031788d3a88967014c8c4f0d82
Dimitri Kurashvili <dimitri@c12.ge>
2013-09-29 20:21:05 +0400

    RS::get_users method

--- lib/rs/sys.rb
+++ lib/rs/sys.rb
@@ -1,5 +1,10 @@
 # -*- encoding : utf-8 -*-
 module RS
+  class User < RS::Initializable
+    attr_accessor :id, :username, :ip, :name, :payer_id
+    def self.extract(data); RS::User.new(id: data[:id].to_i, username: data[:user_name], ip: data[:ip], name: data[:name], payer_id: data[:un_id]) end
+  end
+
   def what_is_my_ip
     waybill_client.call(:what_is_my_ip).body.to_hash[:what_is_my_ip_response][:what_is_my_ip_result]
   end
@@ -16,7 +21,22 @@ module RS
     { user_id: response[:chek_response][:user_id].to_i, s_user_id: response[:chek_response][:sua].to_i } if response[:chek_response][:chek_result]
   end
 
+  def get_users(opts)
+    validate_presence_of(opts, :username, :password)
+    response = waybill_client.call(:get_service_users, message: { 'user_name' => opts[:username], 'user_password' => opts[:password] }).to_hash
+    if response[:get_service_users_response][:get_service_users_result][:service_users].blank?
+      raise 'illegal username/password'
+    else
+      users = []
+      response[:get_service_users_response][:get_service_users_result][:service_users][:service_user].each do |data|
+        users << RS::User.extract(data)
+      end
+      users
+    end
+  end
+
   module_function :what_is_my_ip
   module_function :update_user
   module_function :check_user
+  module_function :get_users
 end
--- test/sys_test.rb
+++ test/sys_test.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 require 'test_helper'
 
-class SystemTest < Test::Unit::TestCase
+class SystemTests < Test::Unit::TestCase
   def test_what_is_my_ip
     ip = RS.what_is_my_ip
     assert ip
@@ -13,5 +13,17 @@ class SystemTest < Test::Unit::TestCase
     data = RS.check_user(TEST_SU)
     assert_equal 783, data[:user_id]
     assert_equal 20155, data[:s_user_id]
+  end
+
+  def test_get_users
+    users = RS.get_users(TEST_USER)
+    assert_equal 165, users.size
+    user = users[0]
+    assert_equal 21111, user.id
+    assert_equal 'TBILISI', user.username
+    assert_equal '31.146.178.142', user.ip
+    refute_nil user.name
+    assert_equal 731937, user.payer_id
+    # {:id=>"21111", :user_name=>"TBILISI", :un_id=>"731937", :ip=>"31.146.178.142", :name=>"parolia: 123456"}
   end
 end
