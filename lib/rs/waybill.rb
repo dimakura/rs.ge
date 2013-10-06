b9bf290dada14893418d49f86e75bbd95238329b
Dimitri Kurashvili <dimitri@c12.ge>
2013-10-06 11:49:46 +0400

    extracting waybill header information

--- lib/rs/waybill.rb
+++ lib/rs/waybill.rb
@@ -14,6 +14,32 @@ module RS
     RETRN = 5
     SUBWB = 6
 
+    WAYBILL_TYPES = {
+      INNER => 'შიდა გადაზიდვა',
+      TRANS => 'ტრანსპორტირებით',
+      WITHOUT_TRANS => 'ტრანსპორტირების გარეშე',
+      DISTR => 'დისტრიბუცია',
+      RETRN => 'უკან დაბრუნება',
+      SUBWB => 'ქვე-ზედნადები'
+    }
+
+    TRANSP_PAID_BY_BUYER  = 1
+    TRANSP_PAID_BY_SELLER = 2
+
+    TRANS_VEHICLE = 1
+    TRANS_RAILWAY = 2
+    TRANS_AIR     = 3
+    TRANS_OTHER   = 4
+    TRANS_VEHICLE_FRGN = 6
+
+    TRANSPORT_TYPES = {
+      TRANS_VEHICLE => 'საავტომობილო',
+      TRANS_RAILWAY => 'სარკინიგზო',
+      TRANS_AIR     => 'საავიაციო',
+      TRANS_OTHER   => 'სხვა',
+      TRANS_VEHICLE_FRGN => 'საავტომობილო - უცხო ქვეყნის'
+    }
+
     attr_accessor :id, :parent_id, :number, :type, :status
     attr_accessor :seller_id, :seller_tin, :seller_name
     attr_accessor :buyer_tin, :check_buyer_tin, :buyer_name
@@ -29,23 +55,24 @@ module RS
     attr_accessor :invoice_id
 
     def self.extract(data)
+      parent_id = data[:par_id].to_i == 0 ? nil : data[:par_id].to_i
       Waybill.new(
         id: data[:id].to_i, type: data[:type].to_i, status: data[:status].to_i,
         seller_id: data[:seler_un_id].to_i, seller_name: data[:seller_name], seller_tin: data[:seller_tin],
-        parent_id: data[:par_id].to_i, number: data[:waybill_number],
-        buyer_tin: data[:buyer_tin], check_buyer_tin: data[:chek_buyer_tin] == '1', buyer_name: data[:buyer_name],
-        driver_tin: data[:driver_tin], check_driver_tin: data[:chek_driver_tin] == '1', driver_name: data[:driver_name],
+        parent_id: parent_id, number: data[:waybill_number],
+        buyer_tin: data[:buyer_tin], check_buyer_tin: data[:chek_buyer_tin].to_i == 1, buyer_name: data[:buyer_name],
+        driver_tin: data[:driver_tin], check_driver_tin: data[:chek_driver_tin].to_i == 1, driver_name: data[:driver_name],
         start_address: data[:start_address], end_address: data[:end_address],
         create_date: data[:create_date], delivery_date: data[:delivery_date],
         activation_date: data[:activate_date], close_date: data[:close_date],
         begin_date: data[:begin_date],
-        transport_cost: data[:transport_coast], amount: data[:full_amount].to_f,
+        transport_cost: data[:transport_coast].to_f, amount: data[:full_amount].to_f,
         transport_cost_payer: data[:tran_cost_payer].to_i,
         transport_type: data[:trans_id].to_i, transport_name: data[:trans_txt],
-        car_number: data[:car_number],
-        su_id: data[:s_user_id],
+        vehicle: data[:car_number],
+        su_id: data[:s_user_id].to_i,
         comment: data[:comment], seller_info: data[:reception_info], buyer_info: data[:receiver_info],
-        confirmation_date: data[:confirmation_date], 
+        confirmed: data[:is_confirmed].to_i == 1, confirmation_date: data[:confirmation_date], 
         invoice_id: (data[:invoice_id].present? ? data[:invoice_id].to_i : nil),
         # items: WaybillItem.extract(data[:goods_list][:goods]),
       )
--- test/waybill_test.rb
+++ test/waybill_test.rb
@@ -6,9 +6,42 @@ class WaybillTest < Test::Unit::TestCase
     waybill = RS.get_waybill(TEST_SU.merge(id: 78061328))
     refute_nil waybill
     assert_equal 78061328, waybill.id
+    assert_nil waybill.parent_id
     assert_equal '0080163857', waybill.number
     assert_equal RS::Waybill::INNER, waybill.type
-    assert_nil waybill.parent_id
-    
+    assert_equal RS::Waybill::DEACTIVATED, waybill.status
+    assert_equal 731937, waybill.seller_id
+    assert_equal '206322102', waybill.seller_tin
+    assert_equal 'სატესტო კოდი', waybill.seller_name
+    assert_equal '206322102', waybill.buyer_tin
+    assert_equal false, waybill.check_buyer_tin, 'XXX: misterious false'
+    assert_nil waybill.buyer_name, 'XXX: misterious nil'
+    assert_equal '35001028457', waybill.driver_tin
+    assert_equal true, waybill.check_driver_tin
+    assert_equal 'იურა აბრამოვი', waybill.driver_name
+    assert_equal 'ასდდფფ გფგჰფ  გფგ', waybill.start_address
+    assert_equal 'აღმაშენებლის 8', waybill.end_address
+    refute_nil waybill.create_date
+    refute_nil waybill.activation_date
+    refute_nil waybill.begin_date
+    assert_nil waybill.delivery_date
+    refute_nil waybill.close_date
+    assert_equal 10, waybill.transport_cost
+    assert_equal RS::Waybill::TRANSP_PAID_BY_BUYER, waybill.transport_cost_payer
+    assert_equal 'IJL652', waybill.vehicle
+    assert_equal RS::Waybill::TRANS_VEHICLE, waybill.transport_type
+    assert_nil waybill.transport_name
+    assert_nil waybill.comment
+    assert_equal 'ზეინკალი, აბრამიძე ტარიელი', waybill.seller_info
+    assert_equal 'ელ.შემდუღებელი, აბუაშვილი შოთა', waybill.buyer_info
+    assert_equal 0, waybill.amount
+    assert_equal 103459, waybill.su_id
+    assert_equal true, waybill.confirmed
+    refute_nil waybill.confirmation_date
+    assert_nil waybill.invoice_id
+    # assert_equal 2, waybill.items.size
+
+    # attr_accessor :confirmed, :confirmation_date
+    # attr_accessor :invoice_id
   end
 end
